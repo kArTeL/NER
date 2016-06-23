5dc50585944ffb51e8c48621e675e903b7aacf38
Neil <ngarcia@soin.co.cr>
2016-06-23 16:58:42 -0600

    added features to the model

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Named Entity Recognition `NER`
-Named Entity Recognition research project
+Named Entity Recognition research project, the main idea is classify entities in classes like:
+Person (PER)
+Location (LOC)
+Organization (ORG)
+Miscellaneous (MISC)
+Other (O)
 
 ### Requirements
 Python 2.7 or above
@@ -14,9 +19,10 @@ Pip for dependencies `python get-pip.py`
 `sudo pip install -r dependencies`
 
 #### Downloading the Corpus `CONLL 2002`
-
+  In this lab we will use the [CONLL 2002 corpus](http://www.cnts.ua.ac.be/conll2002/ner/),
 Execute the file CorpusDownloader.py using `python CorpusDownloader.py`, this will launch a GUI to download the Corpus, select Corpus tab then download the CONLL 2002, then press Download button.
 corpus_downloader
+
 ![alt text](/images/corpus_downloader.png "Corpus downloader")
 
 #### Running the classificator
--- ner.py
+++ ner.py
@@ -5,5 +5,62 @@ from sklearn.preprocessing import LabelBinarizer
 import sklearn
 import pycrfsuite
 
-print(sklearn.__version__)
-print(nltk.corpus.conll2002.fileids())
+# print(sklearn.__version__)
+# print(nltk.corpus.conll2002.fileids())
+train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
+test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
+
+# print(train_sents[0])
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+    features = [
+        'bias',
+        'word.lower=' + word.lower(),
+        'word[-3:]=' + word[-3:],
+        'word[-2:]=' + word[-2:],
+        'word.isupper=%s' % word.isupper(),
+        'word.istitle=%s' % word.istitle(),
+        'word.isdigit=%s' % word.isdigit(),
+        'postag=' + postag,
+        'postag[:2]=' + postag[:2],
+    ]
+    if i > 0:
+        word1 = sent[i-1][0]
+        postag1 = sent[i-1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:word.istitle=%s' % word1.istitle(),
+            '-1:word.isupper=%s' % word1.isupper(),
+            '-1:postag=' + postag1,
+            '-1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('BOS')
+
+    if i < len(sent)-1:
+        word1 = sent[i+1][0]
+        postag1 = sent[i+1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:word.istitle=%s' % word1.istitle(),
+            '+1:word.isupper=%s' % word1.isupper(),
+            '+1:postag=' + postag1,
+            '+1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('EOS')
+
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+print (sent2features(train_sents[0])[0])
