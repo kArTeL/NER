52542bccfde1a09f0c15f985bb836519a8e289d3
Neil García Vargas <Neil@Neils-MacBook-Pro.local>
2016-06-26 21:45:06 -0600

    refactor of classes, now tester.py only test the result with the output of ner.py file

--- ner.py
+++ ner.py
@@ -12,7 +12,7 @@ from subprocess import call
 ##print test data files
 # print(nltk.corpus.conll2002.fileids())
 train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
-test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
+#test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
 
 ## To print the first sentence of the corpus.
 # print(train_sents[0])
@@ -72,9 +72,9 @@ def sent2tokens(sent):
 #%%time
 X_train = [sent2features(s) for s in train_sents]
 y_train = [sent2labels(s) for s in train_sents]
-
-X_test = [sent2features(s) for s in test_sents]
-y_test = [sent2labels(s) for s in test_sents]
+#
+# X_test = [sent2features(s) for s in test_sents]
+# y_test = [sent2labels(s) for s in test_sents]
 
 #To train the model, we create pycrfsuite.Trainer,
 #load the training data and call 'train' method.
@@ -100,63 +100,65 @@ trainer.train('conll2002-esp.crfsuite')
 
 call('ls -lh ./conll2002-esp.crfsuite', shell=True)
 
-tagger = pycrfsuite.Tagger()
-tagger.open('conll2002-esp.crfsuite')
-
-example_sent = test_sents[0]
-print(' '.join(sent2tokens(example_sent)), end='\n\n')
-
-print("Predicted:", ' '.join(tagger.tag(sent2features(example_sent))))
-print("Correct:  ", ' '.join(sent2labels(example_sent)))
-
-def bio_classification_report(y_true, y_pred):
-    """
-    Classification report for a list of BIO-encoded sequences.
-    It computes token-level metrics and discards "O" labels.
-    
-    Note that it requires scikit-learn 0.15+ (or a version from github master)
-    to calculate averages properly!
-    """
-    lb = LabelBinarizer()
-    y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
-    y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
-        
-    tagset = set(lb.classes_) - {'O'}
-    tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
-    class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
-    
-    return classification_report(
-        y_true_combined,
-        y_pred_combined,
-        labels = [class_indices[cls] for cls in tagset],
-        target_names = tagset,
-    )
-    
-
-##time
-y_pred = [tagger.tag(xseq) for xseq in X_test]
-
-print(bio_classification_report(y_test, y_pred))
-
-from collections import Counter
-info = tagger.info()
-
-def print_transitions(trans_features):
-    for (label_from, label_to), weight in trans_features:
-        print("%-6s -> %-7s %0.6f" % (label_from, label_to, weight))
-
-print("Top likely transitions:")
-print_transitions(Counter(info.transitions).most_common(15))
-
-print("\nTop unlikely transitions:")
-print_transitions(Counter(info.transitions).most_common()[-15:])
-
-def print_state_features(state_features):
-    for (attr, label), weight in state_features:
-        print("%0.6f %-6s %s" % (weight, label, attr))    
-
-print("Top positive:")
-print_state_features(Counter(info.state_features).most_common(20))
-
-print("\nTop negative:")
-print_state_features(Counter(info.state_features).most_common()[-20:])
+print("File conll2002-esp.crfsuite created")
+#
+# tagger = pycrfsuite.Tagger()
+# tagger.open('conll2002-esp.crfsuite')
+#
+# example_sent = test_sents[0]
+# print(' '.join(sent2tokens(example_sent)), end='\n\n')
+#
+# print("Predicted:", ' '.join(tagger.tag(sent2features(example_sent))))
+# print("Correct:  ", ' '.join(sent2labels(example_sent)))
+#
+# def bio_classification_report(y_true, y_pred):
+#     """
+#     Classification report for a list of BIO-encoded sequences.
+#     It computes token-level metrics and discards "O" labels.
+#
+#     Note that it requires scikit-learn 0.15+ (or a version from github master)
+#     to calculate averages properly!
+#     """
+#     lb = LabelBinarizer()
+#     y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
+#     y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
+#
+#     tagset = set(lb.classes_) - {'O'}
+#     tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
+#     class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
+#
+#     return classification_report(
+#         y_true_combined,
+#         y_pred_combined,
+#         labels = [class_indices[cls] for cls in tagset],
+#         target_names = tagset,
+#     )
+#
+#
+# ##time
+# y_pred = [tagger.tag(xseq) for xseq in X_test]
+#
+# print(bio_classification_report(y_test, y_pred))
+#
+# from collections import Counter
+# info = tagger.info()
+#
+# def print_transitions(trans_features):
+#     for (label_from, label_to), weight in trans_features:
+#         print("%-6s -> %-7s %0.6f" % (label_from, label_to, weight))
+#
+# print("Top likely transitions:")
+# print_transitions(Counter(info.transitions).most_common(15))
+#
+# print("\nTop unlikely transitions:")
+# print_transitions(Counter(info.transitions).most_common()[-15:])
+#
+# def print_state_features(state_features):
+#     for (attr, label), weight in state_features:
+#         print("%0.6f %-6s %s" % (weight, label, attr))
+#
+# print("Top positive:")
+# print_state_features(Counter(info.state_features).most_common(20))
+#
+# print("\nTop negative:")
+# print_state_features(Counter(info.state_features).most_common()[-20:])
